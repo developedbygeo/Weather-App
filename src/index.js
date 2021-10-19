b378088249d40ba2dc021d1ef96605d387eba8b3
georgiosan <georgiosanagnostou@hotmail.com>
2021-10-20 00:12:01 +0300

    Adding base js file

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './styles/style.scss';
+import '../node_modules/@fortawesome/fontawesome-free/js/all';
+import enableEventListeners from './modules/eventListeners';
+
+enableEventListeners();
