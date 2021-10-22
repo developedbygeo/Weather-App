6ded13a92d8e5257c6a950c2d98fb1c6d3318a92
georgiosan <georgiosanagnostou@hotmail.com>
2021-10-23 00:24:35 +0300

    Cleaned up unused code

--- src/modules/utils.js
+++ src/modules/utils.js
@@ -2,18 +2,15 @@
 function verifyQuery() {
   const queryField = document.querySelector('#location');
   const query = queryField.value;
-  const form = document.querySelector('form');
   const regex = /^[a-zA-Z\s]+$/;
   if (!regex.test(queryField.value)) {
     queryField.setCustomValidity('Please provide a valid city');
     queryField.reportValidity();
   } else {
     queryField.setCustomValidity('');
-    form.reset();
     return query;
   }
 }
-
 function handleBackgroundChanges(newClass) {
   const contentWrapper = document.querySelector('.content-wrapper');
   if (contentWrapper.classList[1]) {
