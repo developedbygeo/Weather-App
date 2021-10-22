5ac771d3b0476fae2bc70d2ba03a6699f2766e7f
georgiosan <georgiosanagnostou@hotmail.com>
2021-10-23 00:23:10 +0300

    Added load animations

--- src/modules/animations.js
+++ src/modules/animations.js
@@ -1,1 +1,40 @@
 import gsap from 'gsap';
+
+export default function loadAnimations() {
+  const timeline = gsap.timeline({
+    defaults: { duration: 2 },
+  });
+
+  timeline
+    .from('.search-wrapper', {
+      y: -100,
+      stagger: 0.5,
+      opacity: 0,
+      ease: 'Power1.easeOut',
+    })
+    .from(
+      'article',
+      {
+        x: -150,
+        opacity: 0,
+        stagger: 0.4,
+        ease: 'slow(0.7, 0.7)',
+      },
+      '-=0.5'
+    )
+    .from(
+      'aside',
+      { x: 150, opacity: 0, ease: 'slow(0.7, 0.7)', stagger: 0.4 },
+      '-=1'
+    )
+    .from(
+      '.img-wrapper',
+      { opacity: 0, x: 150, ease: 'Power0.easeNone' },
+      '-=1'
+    )
+    .from(
+      '.txt-wrapper',
+      { opacity: 0, x: -150, ease: 'Power0.easeNone' },
+      '-=1'
+    );
+}
